80b7457bfe98409394f4dc8201b6a27c183d3e5c
dspain <dmspain@yahoo.com>
2018-06-10 21:00:12 -0700

    add a poll assigns that we can read in the controller

--- vocial/lib/vocial_web/controllers/poll_controller.ex
+++ vocial/lib/vocial_web/controllers/poll_controller.ex
@@ -2,8 +2,17 @@ defmodule VocialWeb.PollController do
   use VocialWeb, :controller
 
   def index(conn, _params) do
+    poll = %{
+      title: "My First Poll",
+      options: [
+        {"Choice 1", 0},
+        {"Choice 2", 5},
+        {"Choice 3", 2}
+      ]
+    }
+
     conn
     |> put_layout(:special)
-    |> render("index.html")
+    |> render("index.html", poll: poll)
   end
 end
