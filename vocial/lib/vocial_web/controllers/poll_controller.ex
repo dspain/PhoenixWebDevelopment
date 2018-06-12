f1cb4e81a42efdc02f91a72520af7df9cf71eaa9
dspain <dmspain@yahoo.com>
2018-06-11 22:49:46 -0700

    use an alias

--- vocial/lib/vocial_web/controllers/poll_controller.ex
+++ vocial/lib/vocial_web/controllers/poll_controller.ex
@@ -1,13 +1,15 @@
 defmodule VocialWeb.PollController do
   use VocialWeb, :controller
 
+  alias Vocial.Votes
+
   def index(conn, _params) do
-    polls = Vocial.Votes.list_polls()
+    polls = Votes.list_polls()
     render(conn, "index.html", polls: polls)
   end
 
   def new(conn, _params) do
-    poll = Vocial.Votes.new_poll()
+    poll = Votes.new_poll()
     render(conn, "new.html", poll: poll)
   end
 
