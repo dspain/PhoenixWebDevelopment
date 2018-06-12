a0e4eb45ad073cc6d1d50719a56dcc9b9b493dfd
dspain <dmspain@yahoo.com>
2018-06-11 22:12:40 -0700

    function to list all polls and corresponding options

--- vocial/lib/vocial/votes/votes.ex
+++ vocial/lib/vocial/votes/votes.ex
@@ -4,4 +4,8 @@ defmodule Vocial.Votes do
   alias Vocial.Repo
   alias Vocial.Votes.Poll
   alias Vocial.Votes.Option
+
+  def list_polls do
+    Repo.all(Poll) |> Repo.preload(:options)
+  end
 end
