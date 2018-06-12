c6678aa8763cf6bcd41c3ddd703af0249b85a6cf
dspain <dmspain@yahoo.com>
2018-06-11 22:23:26 -0700

    basic new action

--- vocial/lib/vocial_web/controllers/poll_controller.ex
+++ vocial/lib/vocial_web/controllers/poll_controller.ex
@@ -8,4 +8,12 @@ defmodule VocialWeb.PollController do
     |> put_layout(:special)
     |> render("index.html", polls: polls)
   end
+
+  def new(conn, _params) do
+    poll = Vocial.Votes.new_poll()
+
+    conn
+    |> put_layout("special.html")
+    |> render("new.html", poll: poll)
+  end
 end
