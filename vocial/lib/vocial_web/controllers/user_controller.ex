f5a8e72185585cf93b07c4fa4a2ae465a6eca111
dspain <dmspain@yahoo.com>
2018-06-12 21:55:44 -0700

    skeleton user controller

---
+++ vocial/lib/vocial_web/controllers/user_controller.ex
@@ -0,0 +1,15 @@
+defmodule VocialWeb.UserController do
+  use VocialWeb, :controller
+
+  def new(conn, _params) do
+    conn
+  end
+
+  def create(conn, _params) do
+    conn
+  end
+
+  def show(conn, _params) do
+    conn
+  end
+end
