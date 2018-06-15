7ccecca936c192e8533d00c37f0d6c06cdcd89e9
dspain <dmspain@yahoo.com>
2018-06-14 21:49:07 -0700

    fill out 'create' action

--- vocial/lib/vocial_web/controllers/session_controller.ex
+++ vocial/lib/vocial_web/controllers/session_controller.ex
@@ -12,5 +12,26 @@ defmodule VocialWeb.SessionController do
     |> redirect(to: "/")
   end
 
-  def create(conn, _params), do: conn
+  def create(conn, %{"username" => username, "password" => password}) do
+    with user <- Accounts.get_user_by_username(username),
+         {:ok, login_user} <- login(user, password) do
+      conn
+      |> put_flash(:info, "Logged in successfully!")
+      |> put_session(:user, %{
+        id: login_user.id,
+        username: login_user.username,
+        email: login_user.email
+      })
+      |> redirect(to: "/")
+    else
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "Invalid username/password")
+        |> render("new.html")
+    end
+  end
+
+  defp login(user, password) do
+    true
+  end
 end
