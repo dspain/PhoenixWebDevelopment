1978023fd5861caf0d83d63ba96d18bea782bb5c
dspain <dmspain@yahoo.com>
2018-06-24 20:07:59 -0700

    fix tests to account for user_id

--- vocial/test/vocial_web/controllers/poll_controller_test.exs
+++ vocial/test/vocial_web/controllers/poll_controller_test.exs
@@ -2,8 +2,16 @@ defmodule VocialWeb.PollControllerTest do
   use VocialWeb.ConnCase
 
   test "GET /polls", %{conn: conn} do
+    {:ok, user} =
+      Vocial.Accounts.create_user(%{
+        username: "test",
+        email: "test@test.com",
+        password: "test",
+        password_confirmation: "test"
+      })
+
     {:ok, poll} =
-      Vocial.Votes.create_poll_with_options(%{title: "Poll 1"}, [
+      Vocial.Votes.create_poll_with_options(%{title: "Poll 1", user_id: user.id}, [
         "Choice 1",
         "Choice 2",
         "Choice 3"
