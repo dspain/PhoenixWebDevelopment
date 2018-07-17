8752f36ed8a8651ce54ea828b58e229ce3962cb7
dspain <dmspain@yahoo.com>
2018-07-16 20:47:59 -0700

    keep track of which ip address already voted

--- vocial/lib/vocial/votes/votes.ex
+++ vocial/lib/vocial/votes/votes.ex
@@ -5,9 +5,10 @@ defmodule Vocial.Votes do
   alias Vocial.Votes.Poll
   alias Vocial.Votes.Option
   alias Vocial.Votes.Image
+  alias Vocial.Votes.VoteRecord
 
   def list_polls do
-    Repo.all(Poll) |> Repo.preload([:options, :image])
+    Repo.all(Poll) |> Repo.preload([:options, :image, :vote_records])
   end
 
   def new_poll do
@@ -56,10 +57,12 @@ defmodule Vocial.Votes do
     Repo.all(Option) |> Repo.preload(:poll)
   end
 
-  def vote_on_option(option_id) do
+  def vote_on_option(option_id, voter_ip) do
     with option <- Repo.get!(Option, option_id),
-         votes <- option.votes + 1 do
-      update_option(option, %{votes: votes})
+         votes <- option.votes + 1,
+         {:ok, option} <- update_option(option, %{votes: votes}),
+         {:ok, _vote_record} <- record_vote(%{poll_id: option.poll_id, ip_address: voter_ip}) do
+      {:ok, option}
     end
   end
 
@@ -69,7 +72,13 @@ defmodule Vocial.Votes do
     |> Repo.update()
   end
 
-  def get_poll(id), do: Repo.get!(Poll, id) |> Repo.preload([:options, :image])
+  def get_poll(id), do: Repo.get!(Poll, id) |> Repo.preload([:options, :image, :vote_records])
+
+  def record_vote(%{poll_id: _poll_id, ip_address: _ip_address} = attrs) do
+    %VoteRecord{}
+    |> VoteRecord.changeset(attrs)
+    |> Repo.insert()
+  end
 
   defp upload_file(%{"image" => image, "user_id" => user_id}, poll) do
     extension = Path.extname(image.filename)
