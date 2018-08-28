2226d3e4f9a96d25a26342127a5049202fc8700e
dspain <dmspain@yahoo.com>
2018-08-27 21:05:12 -0700

    use genserver calls to write & lookup

--- vocial/lib/vocial/chat_cache.ex
+++ vocial/lib/vocial/chat_cache.ex
@@ -12,4 +12,19 @@ defmodule Vocial.ChatCache do
     table = :ets.new(@table, [:bag])
     {:ok, %{table: table}}
   end
+
+  def write(topic, username, status) do
+    row = %{
+      topic: topic,
+      username: username,
+      status: status,
+      timestamp: DateTime.utc_now()
+    }
+
+    GenServer.cast(__MODULE__, {:write, row})
+  end
+
+  def lookup do
+    GenServer.call(__MODULE__, {:lookup})
+  end
 end
