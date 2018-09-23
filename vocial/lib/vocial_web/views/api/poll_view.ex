253c7bbc70f3ea1aca75d1d598f4490cea0ff40d
dspain <dmspain@yahoo.com>
2018-09-22 21:30:01 -0700

    refactor 'render_one'

--- vocial/lib/vocial_web/views/api/poll_view.ex
+++ vocial/lib/vocial_web/views/api/poll_view.ex
@@ -17,16 +17,29 @@ defmodule VocialWeb.Api.PollView do
     %{
       id: poll.id,
       title: poll.title,
-      options: Enum.map(poll.options, fn o -> Map.take(o, [:title, :votes]) end),
-      image: %{
-        url: poll.image.url,
-        alt: poll.image.alt,
-        caption: poll.image.caption
-      }
+      options: render_options(poll.options),
+      image: render_image(poll.image)
     }
   end
 
   def render_many(polls) do
     Enum.map(polls, &render_one/1)
+  end
+
+  def render_options(nil), do: []
+
+  def render_options(options) do
+    options
+    |> Enum.map(fn o -> Map.take(o, [:title, :votes]) end)
+  end
+
+  def render_image(nil), do: nil
+
+  def render_image(image) do
+    %{
+      url: image.url,
+      alt: image.alt,
+      caption: image.caption
+    }
   end
 end
