9cae191f61c0e5991527e3a856c71a45a368f9f8
dspain <dmspain@yahoo.com>
2018-09-22 21:27:43 -0700

    render polls in api poll controller

--- vocial/lib/vocial_web/controllers/api/poll_controller.ex
+++ vocial/lib/vocial_web/controllers/api/poll_controller.ex
@@ -1,7 +1,10 @@
 defmodule VocialWeb.Api.PollController do
   use VocialWeb, :controller
 
+  alias Vocial.Votes
+
   def index(conn, _params) do
-    render(conn, "index.json", [])
+    polls = Votes.list_most_recent_polls()
+    render(conn, "index.json", polls: polls)
   end
 end
--- vocial/lib/vocial_web/views/api/poll_view.ex
+++ vocial/lib/vocial_web/views/api/poll_view.ex
@@ -1,9 +1,32 @@
 defmodule VocialWeb.Api.PollView do
   use VocialWeb, :view
 
+  def render("index.json", %{polls: polls}) do
+    %{
+      polls: render_many(polls)
+    }
+  end
+
   def render("index.json", _data) do
     %{
       message: "Hello World"
     }
+  end
+
+  def render_one(poll) do
+    %{
+      id: poll.id,
+      title: poll.title,
+      options: Enum.map(poll.options, fn o -> Map.take(o, [:title, :votes]) end),
+      image: %{
+        url: poll.image.url,
+        alt: poll.image.alt,
+        caption: poll.image.caption
+      }
+    }
+  end
+
+  def render_many(polls) do
+    Enum.map(polls, &render_one/1)
   end
 end
